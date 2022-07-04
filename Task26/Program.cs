d395b3929a0bb60a261f8c3dd54d66c4fd01b92b
Elena Timofeeva <posazho@gmail.com>
2022-07-04 20:55:51 +0300

    считаем количество цифр в числе

---
+++ Task26/Program.cs
@@ -0,0 +1,17 @@
+﻿// Принимает число и говорит,сколько цифр в числе
+int GetNumberOfNumber (int number1)
+{
+    int count = 0;
+    while (number1 > 0)
+    {
+        number1 = number1 / 10; 
+        count ++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = GetNumberOfNumber(number);
+Console.WriteLine(result);
+
