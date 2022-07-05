80a468316c9cc69f63cce4db9d7ce948fcb14102
Elena Timofeeva <posazho@gmail.com>
2022-07-05 22:44:26 +0300

    Вывод массива рандомом в скобках

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -1,10 +1,39 @@
 ﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
 //[1,0,1,1,0,1,0,0]
 
-int[] array = new int[8];
-for (int i = 0; i < array.Length; i++)
+int[] GetRandomarray()
 {
-    array[i] = new Random().Next(0,2);
+    int[] array = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(0,2);
 
+    }
+    return array;
 }
-Console.WriteLine(string.Join(",", array));
+
+void Printarray(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+       if (i == 0)
+       {
+          Console.Write($"[{array1[i]}, ");
+       }
+       else if (i == array1.Length - 1)
+       {
+            Console.Write($"{array1[i]}]");
+       }
+       else
+       {
+        Console.Write($"{array1[i]}, ");
+       }
+       
+    }
+}
+
+int[] massive = new int [8];
+massive = GetRandomarray();
+Printarray(massive);
+
+//Console.WriteLine(string.Join(",", array));
